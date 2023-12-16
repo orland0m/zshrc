2750d6fa09645e002ebcb93b40b7b960eadd7e6c
sidekick <sidekick@pop-os.localdomain>
2023-12-15 23:50:38 -0800

    Adding default git editor

--- config/once_on_install.zsh
+++ config/once_on_install.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 hash git &>/dev/null && git config --global core.excludesfile $SCRIPT_DIR/config/gitignore_global
+hash git &>/dev/null && hash nvim &>/dev/null && git config --global core.editor nvim
 
 function SuggestBinary() {
     command -v $1 --help &>/dev/null || echo "WARN: $1 is missing from the system, you should install it for better bashrc integration"
