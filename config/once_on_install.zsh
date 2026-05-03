bb4e34a91cde5d9dcd2853165ad1c9e26025bc24
Orlando Miramontes <frizt.orlando@gmail.com>
2026-05-03 14:01:20 -0700

    adding fzf suggest + typo fix

--- config/once_on_install.zsh
+++ config/once_on_install.zsh
@@ -4,10 +4,11 @@ hash git &>/dev/null && git config --global core.excludesfile $SCRIPT_DIR/config
 hash git &>/dev/null && hash nvim &>/dev/null && git config --global core.editor nvim
 
 function SuggestBinary() {
-    command -v $1 --help &>/dev/null || echo "WARN: $1 is missing from the system, you should install it for better bashrc integration"
+    command -v $1 --help &>/dev/null || echo "WARN: $1 is missing from the system, you should install it for better integration"
 }
 
 SuggestBinary "colordiff"
 SuggestBinary "ack"
 SuggestBinary "nvim"
 SuggestBinary "astyle"
+SuggestBinary "fzf"
